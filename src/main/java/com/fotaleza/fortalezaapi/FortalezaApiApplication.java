304e07e9ecdf735a912636d1cea936c71c2f26e3
Miguel Ferreira Moreno <miguelferreira@MacBook-Air-de-Miguel.local>
2024-11-17 22:37:58 -0600

    first commit

---
+++ src/main/java/com/fotaleza/fortalezaapi/FortalezaApiApplication.java
@@ -0,0 +1,13 @@
+package com.fotaleza.fortalezaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortalezaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortalezaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fotaleza/fortalezaapi/FortalezaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fotaleza.fortalezaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortalezaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
