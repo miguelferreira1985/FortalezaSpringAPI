b5b030c834d9f2f4654144207914a3acea177d72
miguelferreira1985 <miguel.fermo1985@gmail.com>
2024-11-19 15:42:31 -0600

    add project authentication

---
+++ src/main/java/com/fotaleza/fortalezaapi/model/ERole.java
@@ -0,0 +1,9 @@
+package com.fotaleza.fortalezaapi.model;
+
+public enum ERole {
+
+    ROLE_CASHIER,
+    ROLE_MANAGER,
+    ROLE_ADMIN
+
+}
---
+++ src/main/java/com/fotaleza/fortalezaapi/payload/response/MessageResponse.java
@@ -0,0 +1,14 @@
+package com.fotaleza.fortalezaapi.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MessageResponse {
+
+    private String message;
+
+}
