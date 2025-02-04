3f3c0f838f2f09ea72752260a777eff4cc91e712
miguelferreira1985 <miguel.fermo1985@gmail.com>
2025-02-04 10:19:31 -0600

    Adding Mappers

--- src/main/java/com/fotaleza/fortalezaapi/controller/CategoryController.java
+++ src/main/java/com/fotaleza/fortalezaapi/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.fotaleza.fortalezaapi.controller;
 
-import com.fotaleza.fortalezaapi.model.Category;
 import com.fotaleza.fortalezaapi.service.impl.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/category")
