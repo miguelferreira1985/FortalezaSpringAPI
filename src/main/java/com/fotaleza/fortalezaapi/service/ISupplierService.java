00d60fbc1f110a2a28dde3d020be0bb607ed8fdc
miguelferreira1985 <miguel.fermo1985@gmail.com>
2025-08-27 17:58:18 -0600

    Repositories refactor

--- src/main/java/com/fotaleza/fortalezaapi/service/ISupplierService.java
+++ src/main/java/com/fotaleza/fortalezaapi/service/ISupplierService.java
@@ -3,15 +3,15 @@ package com.fotaleza.fortalezaapi.service;
 import com.fotaleza.fortalezaapi.model.Supplier;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ISupplierService {
 
-    Supplier saveSupplier(Supplier supplier);
-    List<Supplier> getAllSuppliers(Boolean isActivated);
-    Supplier getSupplierById(Integer supplierId);
-    Supplier getSupplierByName(String supplierName);
-    Boolean existsBySupplierName(String supplierName);
-    Supplier updateSupplier(Supplier supplier);
+    Supplier createSupplier(Supplier supplier);
+    Supplier updateSupplier(Integer supplierId, Supplier supplier);
     void deleteSupplier(Integer supplierId);
+    Optional<Supplier> getSupplierById(Integer supplierId);
+    List<Supplier> getAllSuppliers();
+    List<Supplier> getActiveSuppliers();
 
 }
