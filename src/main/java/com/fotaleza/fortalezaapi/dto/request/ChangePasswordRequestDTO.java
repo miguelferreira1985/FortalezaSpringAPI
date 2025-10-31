3c5ee6329c4a8d8272f775c6599a972a133402e4
miguelferreira1985 <miguel.fermo1985@gmail.com>
2025-10-30 21:46:47 -0600

    fix issues from controller and services

--- src/main/java/com/fotaleza/fortalezaapi/dto/request/ChangePasswordRequestDTO.java
+++ src/main/java/com/fotaleza/fortalezaapi/dto/request/ChangePasswordRequestDTO.java
@@ -1,6 +1,5 @@
 package com.fotaleza.fortalezaapi.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
